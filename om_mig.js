1d88d5405db27d0054e8684af8ec00812b35715c
Annie Li Cao <Annielicao@Annies-MacBook-Air.local>
2021-01-09 21:27:03 +0100

    second commit

---
+++ om_mig.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log(sidenVises);
+
+    document.querySelector("#sky2_sprite").classList.add("floating");
+
+    document.querySelector("#sky3_sprite").classList.add("floating");
+
+
+
+}
+
